1409b371ba668f0b20b815f7a6a62c99943e1798
Gustavo <gus.inacio@hotmail.com>
2022-05-30 09:18:55 -0300

    feat: App router initial template

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,8 @@
 import React from 'react';
-import './App.css';
-import * as io from 'socket.io-client';
-
-const socket = io.connect('http://localhost:3001');
+import Router from 'routes';
 
 function App() {
-  return (
-    <div className="App">
-      oi
-    </div>
-  );
+  return <Router />;
 }
 
 export default App;
