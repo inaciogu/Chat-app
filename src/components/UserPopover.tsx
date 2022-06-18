1eb52f0fc289560f85fac38b9e858bca395b13cb
Gustavo <gus.inacio@hotmail.com>
2022-06-18 18:28:00 -0300

    feat: Logout function + avatar hover effect

--- src/components/UserPopover.tsx
+++ src/components/UserPopover.tsx
@@ -4,13 +4,29 @@ import {
   ListItemIcon,
 } from '@mui/material';
 import useAccount from 'hooks/useAccount';
-import { useRef, useState } from 'react';
+import { useLayoutEffect, useRef, useState } from 'react';
+import { Navigate } from 'react-router-dom';
 
 export default function UserPopover() {
   const { username } = useAccount();
-  const anchorRef = useRef<HTMLButtonElement>({} as HTMLButtonElement);
+  const anchorRef = useRef(null);
+  const avatarRef = useRef<HTMLDivElement>({} as HTMLDivElement);
 
   const [open, setOpen] = useState<boolean>(false);
+  const [variant, setVariant] = useState<any>('circular');
+
+  const logout = () => <Navigate to="/" replace />;
+
+  useLayoutEffect(() => {
+    avatarRef.current.onmouseenter = () => {
+      setVariant('rounded');
+    };
+
+    avatarRef.current.onmouseleave = () => {
+      setVariant('circular');
+    };
+  }, [avatarRef]);
+
   return (
     <>
       <IconButton
@@ -18,12 +34,12 @@ export default function UserPopover() {
         onClick={() => setOpen(true)}
         sx={{ mr: 2 }}
       >
-        <Avatar alt={username} />
+        <Avatar ref={avatarRef} variant={variant} alt={username} />
       </IconButton>
-      <Popover anchorOrigin={{ vertical: 'bottom', horizontal: 'left' }} anchorEl={anchorRef.current} open={open} onClose={() => setOpen(false)}>
+      <Popover anchorOrigin={{ vertical: 'bottom', horizontal: 'left' }} anchorEl={anchorRef?.current} open={open} onClose={() => setOpen(false)}>
         <Card>
           <List>
-            <ListItemButton>
+            <ListItemButton onClick={logout}>
               <ListItemIcon>
                 <LogoutIcon />
               </ListItemIcon>
