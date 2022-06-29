f15e68d6a5a74afde6fca96ecb927e63848dbd56
Gustavo <gus.inacio@hotmail.com>
2022-06-29 12:51:17 -0300

    feat: auth reducer to manage the states

--- src/contexts/UserContext.tsx
+++ src/contexts/UserContext.tsx
@@ -1,11 +1,7 @@
 import {
-  Gamepad, Help, MeetingRoom,
-  Work,
-} from '@mui/icons-material';
-import {
-  createContext, ReactNode, useEffect, useState,
+  createContext, ReactNode, useEffect, useReducer, useState,
 } from 'react';
-import { TUserRegister } from 'services/auth.service';
+import { LOGIN, TUserLogin, TUserRegister } from 'services/auth.service';
 import { GET_ROOMS } from 'services/rooms.service';
 import * as io from 'socket.io-client';
 
@@ -27,13 +23,64 @@ interface IUserProvider {
   children: ReactNode
 }
 
+enum EActionTypes {
+  Initial = 'INITIAL',
+  Login = 'LOGIN',
+  Logout = 'LOGOUT'
+}
+
+type TAuthAction = {
+  type: EActionTypes;
+  payload: {
+    isAuthenticated: boolean;
+    user: TUser
+  }
+}
+
+type TAuthState = {
+  isAuthenticated: boolean;
+  isInitialized: boolean;
+  user: TUser | null;
+}
+
+const initialState: TAuthState = {
+  isInitialized: false,
+  isAuthenticated: false,
+  user: null,
+};
+
+const authReducer = (state: TAuthState, action: TAuthAction) => {
+  switch (action.type) {
+    case EActionTypes.Initial:
+      return {
+        isInitialized: true,
+        isAuthenticated: action.payload.isAuthenticated,
+        user: action.payload.user,
+      };
+    case EActionTypes.Login:
+      return {
+        ...state,
+        isAuthenticated: true,
+        user: action.payload.user,
+      };
+    case EActionTypes.Logout:
+      return {
+        ...state,
+        isAuthenticated: false,
+        user: null,
+      };
+    default:
+      return state;
+  }
+};
+
 export const userContext = createContext({} as TUserContext);
 
 const socket = io.connect(process.env.REACT_APP_API_URL || '', { reconnection: true, transports: ['websocket'] });
 
 export default function UserProvider({ children }: IUserProvider) {
+  const [state, dispatch] = useReducer(authReducer, initialState);
   const [username, setUsername] = useState<string>('');
-  const [user, setUser] = useState<TUser | null>(null);
   const [rooms, setRooms] = useState<IRoom[]>([]);
 
   const handleUsername = (value: string) => {
@@ -51,6 +98,10 @@ export default function UserProvider({ children }: IUserProvider) {
     };
     getAvailableRooms();
   }, []);
+
+  /* const login = async (body: TUserLogin) => {
+    const { data } = await LOGIN(body);
+  } */
 
   return (
     <userContext.Provider value={{
